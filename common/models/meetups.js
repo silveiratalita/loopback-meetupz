85dfd6b06d0cbaeeb93a7b8ce9b83399fbd0beec
Talita <silveira.tacristina@gmail.com>
2021-04-08 19:52:18 -0300

    salvando um meetup

--- common/models/meetups.js
+++ common/models/meetups.js
@@ -10,10 +10,42 @@ module.exports = function(Meetups) {
         {arg: 'value', type: 'string'}
         ]
     });
+    Meetups.remoteMethod('createMeetup',{
+        http: {path: '/createMeetup', verb: 'post'},
+        accepts: [
+            {arg: 'body', type: 'object', required: true, http: {source: 'body'}}
+        ],
+        returns: [
+        {arg: 'value', type: 'object'}
+        ]
+    });
+    
+//cria um meetup
 
-    Meetups.concat = function(input, cb){
+//   const meetupCreated =  Meetups.create({city: 'foo@bar.com', password: 'bar'}, function(err, userInstance) {
+//       console.log(userInstance);
+  
+Meetups.createMeetup = function(body,cb){
+    const meetupCreated =  Meetups.create(
+        {city: body.city, adress: body.adress, name:body.name},
+        function(err, cbk){
+            if(err){
+                throw new Error(err);
+        }else{
+            console.log(cbk)
+        }
+    }
+        );
+        cb(null, meetupCreated);
+
+}
+
+
+
+    Meetups.concat = function(body, cb){
+
         console.log('teste')
-        cb(null, `${input.city}-${input.adress}`);
+        cb(null, `${body.city}-${body.adress}`);
        
     }
 
